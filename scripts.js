0c8780d6ef2411ede9e1c7cad264593ce707afc0
Maximilian McClelland <mwmttl@gmail.com>
2025-01-23 10:45:32 -0700

    site Basics

---
+++ scripts.js
@@ -0,0 +1,43 @@
+/**
+ * On page load, pick the appropriate random background image
+ * based on the current viewport orientation (width vs height).
+ */
+window.addEventListener("load", function () {
+    setRandomBackground();
+});
+
+/**
+ * Chooses a random wallpaper from either ~/walls/H/ or ~/walls/V/
+ * (images 1.png to 6.png) based on the orientation.
+ */
+function setRandomBackground() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    let folder = "";
+
+    if (width > height) {
+        // Landscape orientation -> use "H"
+        folder = "H";
+    } else {
+        // Portrait orientation -> use "V"
+        folder = "V";
+    }
+
+    const randomIndex = Math.floor(Math.random() * 6) + 1; // 1 to 6
+    const bgDiv = document.getElementById("bg");
+    bgDiv.style.backgroundImage = `url('/walls/${folder}/${randomIndex}.png')`;
+}
+
+/**
+ * Redirect to donglecraft.com if the DongleCraft image is clicked.
+ */
+function redirectHome() {
+    window.location.href = "https://donglecraft.com";
+}
+
+/**
+ * Navigate to instructions.html from the index page.
+ */
+function goInstructions() {
+    window.location.href = "instructions.html";
+}
